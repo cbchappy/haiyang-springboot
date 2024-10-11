f930a76c466676df5788ca71889751a74347a067
cbc <cbc@git.com>
2024-10-11 11:46:54 +0800

    Initial commit

---
+++ src/main/java/com/example/haiyang/HaiyangApplication.java
@@ -0,0 +1,13 @@
+package com.example.haiyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaiyangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/haiyang/HaiyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haiyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaiyangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
